b3e362938ff91d8d1199c9c7d077ea9dcb716d69
Benjamín Arauz <111297680+BenjaminArauz@users.noreply.github.com>
2023-06-02 23:39:27 -0600

    Add files via upload

--- Task.cpp
+++ Task.cpp
@@ -1,61 +1,66 @@
+/*
+La clase Event, la cual va a ser crucial para el objeto tipo Agenda, 
+representa agregación en la Agenda
+*/
+
+//Incluimos el archivo.h
 #include "Task.h"
 
+//Librerias necesarias para esta clase
 #include <iostream>
 #include <string>
 using namespace std;
 
+//Constructores
 Task::Task(){
     priority = 0;
     complete = false;
 }
-Task::Task(string _title, int _priority, bool _complete){
+Task::Task(string _title, int _priority, bool _complete, int day, int month, int year){
     title = _title;
     priority = _priority;
     complete = _complete;
+    //Date es composición de Task, por lo que se crea dentro del constructor tipo Task
+    Date date1(day, month, year);
+    deliverDate = date1;
 }
-string Task::getTitle(){
-    return title;
-}
+//Getters
 int Task::getPriority(){
     return priority;
 }
 bool Task::getComplete(){
     return complete;
 }
-void Task::setTitle(string _title){
-    title = _title;
+Date Task::getDeliverDate(){
+    return deliverDate;
 }
+//Setters
 void Task::setPriority(int _priority){
     priority = _priority;
 }
 void Task::setComplete(bool _complete){
     complete = _complete;
 }
-void Task::printInformationTask(){
-    cout << "Title: " << getTitle() << endl;
-    cout << "Priority: ";
-    switch (getPriority()){
-        case 1:
-            cout << "1. High" << endl;
-            break;
-        case 2:
-            cout << "2. Medium" << endl;
-            break;
-        case 3:
-            cout << "3. Low" << endl;
-            break;
-        default: 
-            break;
+void Task::setDeliverDate(Date _deliverDate){
+    deliverDate = _deliverDate;
+}
+//Método extra para imprimir toda la información de Task
+string Task::printInformationTask(){
+    string msg;
+    msg = "Title: " + title + "\n";
+    msg += "Priority: ";
+    if (getPriority() == 1){
+        msg += "High \n";
+    } else if (getPriority() == 2){
+        msg += "Medium \n";
+    } else if (getPriority() == 3){
+        msg += "Low \n";
     }
-    cout << "Process: ";
-    switch (getComplete()){
-        case true:
-            cout << "Finished" << endl;
-            break;
-        case false:
-            cout << "Pendient" << endl;
-            break;
-        default:
-            break;
+    msg += "Process: ";
+    if (getComplete() == true){
+        msg += "Finished \n";
+    } else if (getComplete() == false){
+        msg += "Pendient \n";
     }
+    return msg;
 }
