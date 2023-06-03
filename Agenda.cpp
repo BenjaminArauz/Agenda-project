1b94a3556f3cb9f09d67c3570dff04fa7006a9c5
Benjamín Arauz <111297680+BenjaminArauz@users.noreply.github.com>
2023-06-02 23:36:05 -0600

    Tercer avance

--- Agenda.cpp
+++ Agenda.cpp
@@ -1,54 +1,92 @@
+/*
+La clase Teacher, representa a un tipo de la clase Persona, de la misma manera,
+que la clase de tipo Student, estamos representando herencia. Tiene tres atributos
+extras que le diferencian de los demás.
+*/
+
+//Incluimos el archivo.h
 #include "Agenda.h"
 
+//Librerias necesarias para esta clase
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 
+//Constructores
 Agenda::Agenda(){}
 Agenda::Agenda(vector<Task> _task, vector<Event> _event){
     task = _task;
     event = _event;
 }
-Task Agenda::getTask(int position){
-    return task[position];
+Agenda::Agenda(vector<Task> _task){
+    task = _task;
+    vector<Event> emptyEvents;
+    event = emptyEvents;
 }
-Event Agenda::getEvent(int position){
-    return event[position];
+Agenda::Agenda(vector<Event> _event){
+    event = _event;
+    vector<Task> emptyTask;
+    task = emptyTask;
 }
-void Agenda::setTask(Task _task, int positionTask){
-    task[positionTask] = _task;
+//Getters
+vector<Task> Agenda::getTask(){
+    return task;
 }
-void Agenda::setEvent(Event _event, int positionEvent){
-    event[positionEvent] = _event;
+vector<Event> Agenda::getEvent(){
+    return event;
 }
+//Setters
+void Agenda::setTask(vector <Task> _task){
+    task = _task;
+}
+void Agenda::setEvent(vector <Event> _event){
+    event = _event;
+}
+//Funciones adicionaless
 int Agenda::lenTask(){
     return task.size();
 }
 int Agenda::lenEvent(){
     return event.size();
 }
-void Agenda::addTask(){
-    string title;
-    int priority;
-    bool complete;
-    cout << "Title: ";
-    cin >> title;
-    cout << "Priority (1. High, 2. Medium, 3. Low): ";
-    cin >> priority;
-    Task task2(title, priority, complete);
-    task.push_back(task2);
+void Agenda::addTask(Task task1){
+    task.push_back(task1);
+}
+void Agenda::addEvent(Event event1){
+    event.push_back(event1);
 }
 void Agenda::deleteTask(int position){
     task.erase(task.begin() + (position - 1));
 }
-void Agenda::printInformation(){
-    cout << "Task: " << endl;
-    for (int i = 0; i < task.size(); i++){
-        task[i].printInformationTask();
+void Agenda::deleteEvent(int position){
+    event.erase(event.begin() + (position - 1));   
+}
+string Agenda::printInformation(){
+    string msg;
+    msg = "Task: \n";
+    if (lenTask() != 0){
+        for (int i = 0; i < task.size(); i++){
+            msg += to_string(i + 1) + ". \n";
+            msg += task[i].printInformationTask();
+        }
+    } else {
+        msg += "There is no task agended \n";
     }
-    cout << "Event: " << endl;
-    for (int i = 0; i < event.size(); i++){
-        event[i].printInformationEvent();
+    msg += "Event: \n";
+    if (lenEvent() != 0){
+        for (int i = 0; i < event.size(); i++){
+            msg += to_string(i + 1) + ". \n";
+            msg += event[i].printInformationEvent();
+        }
+    } else {
+        msg += "There is no events agended \n";
     }
+    return msg;
+}
+Task Agenda::getTaskPosition(int position){
+    return task[position];
+}
+Event Agenda::getEventPosition(int position){
+    return event[position];
 }
