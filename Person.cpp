17d8fd99d1488362fe6be59f93aa270f37c649dd
Benjamín Arauz <111297680+BenjaminArauz@users.noreply.github.com>
2023-06-02 23:37:45 -0600

    Tercer avance

--- Person.cpp
+++ Person.cpp
@@ -1,26 +1,54 @@
+/*
+La clase Persona, es una clase abstracta que va a representar al 
+usuario.
+*/
+
+//Incluimos el archivo .h
 #include "Person.h"
 
+//Librerias necesarias para esta clase
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 
+//Constructores
 Person::Person(){
     name = " ";
     ID = " ";
     email = " ";
-    phone = 0;
+    username = " ";
+    phone = "0";
 }
-Person::Person(string _name, string _ID, string _email, int _phone, vector<Task> tasks, vector<Event> events){
+Person::Person(string _name, string _ID, string _email, string _username, string _phone, vector<Task> tasks, vector<Event> events){
     name = _name;
     ID = _ID;
     email = _email;
+    username = _username;
     phone = _phone;
     agenda = new Agenda(tasks, events);
 }
+Person::Person(string _name, string _ID, string _email, string _username, string _phone, vector<Task> tasks){
+    name = _name;
+    ID = _ID;
+    email = _email;
+    username = _username;
+    phone = _phone;
+    agenda = new Agenda(tasks);
+}
+Person::Person(string _name, string _ID, string _email, string _username, string _phone, vector<Event> events){
+    name = _name;
+    ID = _ID;
+    email = _email;
+    username = _username;
+    phone = _phone;
+    agenda = new Agenda(events);
+}
+//Destructor
 Person::~Person(){
     delete agenda;
 }
+//Getters
 string Person::getName(){
     return name;
 }
@@ -30,12 +58,16 @@ string Person::getID(){
 string Person::getEmail(){
     return email;
 }
-int Person::getPhone(){
+string Person::getUsername(){
+    return username;
+}
+string Person::getPhone(){
     return phone;
 }
 Agenda *Person::getAgenda(){
     return agenda;
 }
+//Setters
 void Person::setName(string _name){
     name = _name;
 }
@@ -45,9 +77,12 @@ void Person::setID(string _ID){
 void Person::setEmail(string _email){
     email = _email;
 }
-void Person::setPhone(int _phone){
+void Person::setUsername(string _username){
+    username = _username;
+}
+void Person::setPhone(string _phone){
     phone = _phone;
 }
-void *Person::setAgenda(Agenda *_agenda){
+void Person::setAgenda(Agenda *_agenda){
     agenda = _agenda;
 }
