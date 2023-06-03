65763f30475a42e1cfc6d218f3cb0e2f9c96ba58
Benjamín Arauz <111297680+BenjaminArauz@users.noreply.github.com>
2023-06-02 23:41:47 -0600

    Tercer avance

--- Teacher.h
+++ Teacher.h
@@ -1,27 +1,46 @@
+/*
+La clase Teacher, representa a un tipo de la clase Persona, de la misma manera,
+que la clase de tipo Student, estamos representando herencia. Tiene tres atributos
+extras que le diferencian de los demás.
+*/
+
 #ifndef TEACHER_H
 #define TEACHER_H
 
+//Librerias necesarias para esta clase
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 
+//Clase que se va a incluir
 #include "Person.h"
 
-class Teacher : public Person {
+//Definición de la clase
+class Teacher : public Person { //Representamos herencia de Persona
     private:
         string subject;
         string degree;
         int yearsExperience;
     public:
+        //Constructores
         Teacher();
-        Teacher(string, string, string, int, vector<Task>, vector<Event>, string, int, string);
+        //Agregamos los atributos de la clase padre
+        Teacher(string, string, string, string, string, vector<Task>, vector<Event>, string, string, int);
+        Teacher(string, string, string, string, string, vector<Task>, string, string, int);
+        Teacher(string, string, string, string, string, vector<Event>, string, string, int);
+        //Destructor
+        ~Teacher();
+        //Getters
         string getSubject();
         int getYearsExperience();
         string getDegree();
+        //Setters
         void setSubject(string);
         void setYearsExperience(int);
         void setDegree(string);
+        //Función sobreescrita de la clase Padre
+        string printInformationPerson() override;
 };
 
 #endif
