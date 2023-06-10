5c7059f0acafb38bb67c0652edafd1aa9a0243b0
Benjamín Arauz <111297680+BenjaminArauz@users.noreply.github.com>
2023-06-10 11:11:31 -0600

    Último avance

--- Person.cpp
+++ Person.cpp
@@ -1,16 +1,63 @@
 /*
-La clase Persona, es una clase abstracta que va a representar al 
-usuario.
+*
+*
+* Agenda project
+* Benjamín Iván Arauz Córdova
+* 10/06/2023
+* Dentro de este proyecto se ha implementado la clase abstracta Persona, la cual tiene un rol fundamental al repre-
+* sentar los usuarios que interáctuan con el sistema. Sireve como una base concceptual para definir las caracterís-
+* ticas y comportamientos comunes que comparten tanto Student como Teacher. Al ser una clase abstracta no se puede
+* instanciar directamente lo cual nos sirve para poder aplicar polimorfismo en el main. 
 */
 
-//Incluimos el archivo .h
-#include "Person.h"
+#ifndef PERSON_CPP
+#define PERSON_CPP
 
 //Librerias necesarias para esta clase
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
+
+//Clase que se van a incluir
+#include "Agenda.cpp"
+
+//Definicion de clase
+class Person {
+    protected:
+        string name;
+        string ID;
+        string email;
+        string username;
+        string phone;
+        Agenda *agenda;
+    public:
+        //Constructores
+        Person();
+        //Declaramos sobreeescritura con los constructores
+        //Constructor con un vector de objetos tipo Task
+        Person(string, string, string, string, string, vector<Task>);
+        //Constructor con un vector de objetos tipo Event
+        Person(string, string, string, string, string, vector<Event>);
+        //Destructor
+        ~Person();
+        //Getters
+        string getName();
+        string getID();
+        string getEmail();
+        string getUsername();
+        string getPhone();
+        Agenda *getAgenda();
+        //Setters
+        void setName(string);
+        void setID(string);
+        void setEmail(string);
+        void setUsername(string);
+        void setPhone(string);
+        void setAgenda(Agenda*);
+        //Método virtual puro que declara a la clase Persona como abstracta
+        virtual string printInformationPerson() = 0;
+};
 
 //Constructores
 Person::Person(){
@@ -19,14 +66,6 @@ Person::Person(){
     email = " ";
     username = " ";
     phone = "0";
-}
-Person::Person(string _name, string _ID, string _email, string _username, string _phone, vector<Task> tasks, vector<Event> events){
-    name = _name;
-    ID = _ID;
-    email = _email;
-    username = _username;
-    phone = _phone;
-    agenda = new Agenda(tasks, events);
 }
 Person::Person(string _name, string _ID, string _email, string _username, string _phone, vector<Task> tasks){
     name = _name;
@@ -86,3 +125,5 @@ void Person::setPhone(string _phone){
 void Person::setAgenda(Agenda *_agenda){
     agenda = _agenda;
 }
+//Obs: No se declara el cuerpo de la función printInformationPerson, debido a que es abstracto
+#endif
