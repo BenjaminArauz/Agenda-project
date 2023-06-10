ec1ee90d9791a0a527d04b6afa31a5fb4e7c94e0
Benjamín Arauz <111297680+BenjaminArauz@users.noreply.github.com>
2023-06-10 11:10:03 -0600

    Último avance

--- Date.cpp
+++ Date.cpp
@@ -1,17 +1,46 @@
 /*
-Definimos la clase Date, que nos ayuda a definir el día de entrega de 
-un objeto tipo Task y ayuda a definir el día del evento para un objeto
-tipo Event.
+*
+*
+* Agenda project
+* Benjamín Iván Arauz Córdova
+* 10/06/2023
+* La clase Date reepresenta la fecha, nos permite definir y manipular fechas, resulta fundamental para establecer
+* tanto las fechas de las tareas y las fechas de los evento. Brinda una mayor organización y seguimiento de las
+* actividades pendientes. La clase Date tiene 3 atributos, de la misma manera, en esta clase, se le aplica sobre-
+* carga de métodos, a +. 
 */
 
-//Incluimos el archivo .h
-#include "Date.h"
+#ifndef DATE_CPP
+#define DATE_CPP
 
 //Librerias necesarias para esta clase
 #include <iostream>
 #include <string>
-#include <vector>
 using namespace std;
+
+//Definición de la clase 
+class Date {
+    private:
+        int day;
+        int month;
+        int year;
+    public:
+        //Constructores
+        Date();
+        Date(int, int, int);
+        //Getters
+        int getDay();
+        int getMonth();
+        int getYear();
+        //Setters
+        void setDay(int);
+        void setMonth(int);
+        void setYear(int);
+        //Métodos adicionales
+        string printDate(); //Imprime datos
+        //Sobrecarga de operador +
+        friend string operator+(string, Date);
+};
 
 //Constructores
 Date::Date(){
@@ -44,3 +73,15 @@ void Date::setMonth(int _month){
 void Date::setYear(int _year){
     year = _year;
 }
+//Funciones adicionales
+string Date::printDate(){
+    string msg;
+    msg = to_string(day) + "-" + to_string(month) + "-" + to_string(year);
+    return msg;
+}
+//Sobrecarga de opeereador + 
+string operator+(string msg, Date date1){
+    return msg + date1.printDate();
+}
+
+#endif
